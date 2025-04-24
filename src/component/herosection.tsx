de5779dd416833c1cce56ea6374302ca5ac79d37
Chirag-Jamariya <chiragjamariya01@gmail.com>
2025-04-24 18:16:26 +0530

    herosection/faqsection changes1

--- src/component/herosection.tsx
+++ src/component/herosection.tsx
@@ -10,7 +10,6 @@ export default function HeroSection() {
             setShowText(true);  
         }, 100);
         return () => {
-            
             clearTimeout(textTimer);
         };
     }, []);
@@ -26,15 +25,13 @@ export default function HeroSection() {
             <div className={`flex flex-col items-start justify-center h-full bg-opacity-40 transition-opacity duration-[1000ms] ease-in-out ${
                 textVisible.current ? 'opacity-100' : 'opacity-0' 
             } z-10 relative pl-40`}>
-                <h1 className="text-7xl font-semibold text-white">WELCOME TO V-NEST</h1>
-                <p className="mt-4 text-4xl  text-gray-300">VIT Chennai Startups and Research</p>
-                <p className=" text-4xl  text-gray-300">Foundation</p>
-                <p className="mt-3 text-4xl  text-gray-300">We Nurture You to Fly High</p>
-                <button className="mt-4 px-4 py-2 h-14 w-56 bg-gradient-to-r from-blue-500 to-purple-500 text-white text-2xl rounded transition-colors ease-in-out duration-1000 hover:bg-gradient-to-r hover:from-purple-500 hover:to-blue-500">
+                <h1 className="text-7xl text-white fancy-text">WELCOME TO V-NEST</h1>
+                <p className="mt-4 text-4xl text-gray-300 fancy-text">VIT Chennai Startups and Research</p>
+                <p className="text-4xl text-gray-300 fancy-text">Foundation</p>
+                <p className="mt-3 text-4xl text-gray-300 fancy-text">We Nurture You to Fly High</p>
+                <button className="mt-4 px-4 py-2 h-14 w-56 bg-gradient-to-r from-blue-500 to-purple-500 text-white text-2xl rounded transition-colors ease-in-out duration-1000 hover:bg-gradient-to-r hover:from-purple-500 hover:to-blue-500 fancy-text">
                     Apply Now
                 </button>
-
-
             </div>
         </section>
     );
