245bac5eb834358c989bbd326a7624babe05ea7a
Yas1th005 <yashpanatala@gmail.com>
2025-04-25 05:06:00 +0530

    Final

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  images: { unoptimized: true },
+  /* config options here */
 };
 
 export default nextConfig;
